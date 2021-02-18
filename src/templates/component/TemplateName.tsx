37af4b5661142c26f3742f1a90069d0b086b2fc1
冉宁可 <rnk19@mails.tsinghua.edu.cn>
2021-02-18 23:58:37 +0800

    update component template

--- src/templates/component/TemplateName.tsx
+++ src/templates/component/TemplateName.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import './TemplateName.css';
 
 interface Props {}
 
 const TemplateName: React.FC<Props> = (_props) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { t } = useTranslation();
     // prettier-ignore
     return (
-        <div className="TemplateName">
+        <Box className="TemplateName">
 
-        </div>
+        </Box>
     );
 };
 
