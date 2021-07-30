9b5abd4e8ee7832a970c61c556b60dafb03ec117
nramos0 <xoen000@163.com>
2021-07-31 05:51:29 +0800

    fix missing loading indicator

--- src/components/DataFetch/DataFetch.tsx
+++ src/components/DataFetch/DataFetch.tsx
@@ -4,6 +4,7 @@ import { useGetWordData } from '../../net/requests';
 import { useGetUser } from '../../net/requests/getUser';
 import { useStore } from '../../hooks/useStore';
 import i18n, { i18nInitPromise } from '../../i18n';
+import { useLoadInfo } from '../general/LoadWrapper/LoadWrapper';
 
 interface Props {
     fetch: boolean;
@@ -15,29 +16,32 @@ const DataFetch: React.FC<Props> = ({ fetch, setHasData }) => {
 
     const { fetch: fetchWordData } = useGetWordData();
     const { fetch: fetchUser } = useGetUser();
+    const loadInfo = useLoadInfo();
 
     useEffect(() => {
         if (fetch) {
-            Promise.all([fetchWordData(), fetchUser()])
-                .then(([wordData, user]) => {
-                    if (wordData === null || user === null) {
-                        throw new Error('data is null');
-                    }
+            loadInfo.loadUntilResolve(
+                Promise.all([fetchWordData(), fetchUser()])
+                    .then(([wordData, user]) => {
+                        if (wordData === null || user === null) {
+                            throw new Error('data is null');
+                        }
 
-                    store.setWordData(wordData);
-                    store.setUser(user);
-                    i18nInitPromise.then(() => {
-                        i18n.changeLanguage(user.display_lang);
-                    });
+                        store.setWordData(wordData);
+                        store.setUser(user);
+                        i18nInitPromise.then(() => {
+                            i18n.changeLanguage(user.display_lang);
+                        });
 
-                    setHasData(true);
-                })
-                .catch(() => {
-                    // do something
-                    console.error('data was null');
-                });
+                        setHasData(true);
+                    })
+                    .catch(() => {
+                        // do something
+                        console.error('data was null');
+                    })
+            );
         }
-    }, [fetch, fetchUser, fetchWordData, setHasData, store]);
+    }, [fetch, fetchUser, fetchWordData, loadInfo, setHasData, store]);
 
     return null;
 };
