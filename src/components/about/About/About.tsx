f8ab227666c605cb08fb9fe45047286df62a426a
nramos0 <xoen000@163.com>
2021-08-06 14:37:05 +0800

    improve about page translation implementation

--- src/components/about/About/About.tsx
+++ src/components/about/About/About.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Flex, Heading, Text, Box, Link } from '@chakra-ui/react';
-import { useTranslation } from 'react-i18next';
+import { Flex, Heading, Text, Box, Link as ChakraLink } from '@chakra-ui/react';
+import { useTranslation, Trans } from 'react-i18next';
 import ScrollBox from '../../general/ScrollBox/ScrollBox';
 
 const line = (
     <Box width="100%" height="1px" bgColor="#d16161" borderRadius="5px" />
 );
+
+const Link: React.FC<{ href: string; bold?: boolean }> = ({
+    href,
+    bold,
+    children,
+}) => {
+    return (
+        <ChakraLink
+            target="_blank"
+            rel="noreferer noopener"
+            href={href}
+            fontWeight={bold ? 'bold' : 'normal'}
+        >
+            {children}
+        </ChakraLink>
+    );
+};
+
+const appDescComponents = [
+    <Link href="https://github.com/nramos0/fluent-reader/" bold={true} />,
+    <Link href="https://reactjs.org/" bold={true} />,
+    <Link href="https://www.rust-lang.org/" bold={true} />,
+];
+
+const devCreditComponents = [
+    <Link href="mailto:xoen000@163.com" />,
+    <Link href="https://github.com/nramos0/" />,
+];
 
 const About: React.FC = () => {
     const { t } = useTranslation('about');
@@ -28,34 +56,11 @@ const About: React.FC = () => {
                     <Text mb={2}>{t('app-description-1')}</Text>
                     {line}
                     <Text mt={2} mb={2}>
-                        {t('app-description-2-1')}
-                        <Link
-                            target="_blank"
-                            rel="noreferer noopener"
-                            href="https://github.com/nramos0/fluent-reader/"
-                            fontWeight="bold"
-                        >
-                            {t('app-code-link')}
-                        </Link>
-                        {t('app-description-2-2')}
-                        <Link
-                            target="_blank"
-                            rel="noreferer noopener"
-                            href="https://reactjs.org/"
-                            fontWeight="bold"
-                        >
-                            {t('react')}
-                        </Link>
-                        {t('app-description-2-3')}
-                        <Link
-                            target="_blank"
-                            rel="noreferer noopener"
-                            href="https://www.rust-lang.org/"
-                            fontWeight="bold"
-                        >
-                            {t('rust')}
-                        </Link>
-                        {t('app-description-2-4')}
+                        <Trans
+                            i18nKey="app-description-2"
+                            ns="about"
+                            components={appDescComponents}
+                        />
                     </Text>
 
                     <Heading mb={2} fontSize="20px" textAlign="center">
@@ -63,25 +68,17 @@ const About: React.FC = () => {
                     </Heading>
                     <Text>{t('credits-1')}</Text>
                     <Text>
-                        {t('developer')}: {t('nicholas')}
-                        {' | '}
-                        <Text as="span">{t('email')}: </Text>
-                        <Link href="mailto:xoen000@163.com">
-                            xoen000@163.com
-                        </Link>
-                        {' | '}
-                        <Text as="span">{t('github')}: </Text>
-                        <Link
-                            href="https://github.com/nramos0/"
-                            target="_blank"
-                            rel="noreferer noopener"
-                        >
-                            https://github.com/nramos0/
-                        </Link>
+                        <Trans
+                            i18nKey="dev-credit"
+                            ns="about"
+                            components={devCreditComponents}
+                            values={{
+                                email: 'xoen000@163.com',
+                                github: 'https://github.com/nramos0/',
+                            }}
+                        />
                     </Text>
-                    <Text>
-                        {t('designer')}: {t('porter')}
-                    </Text>
+                    <Text>{t('designer-credit')}</Text>
                 </Flex>
             </ScrollBox>
         </Flex>
