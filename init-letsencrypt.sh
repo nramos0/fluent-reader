5902fd390baae69d53d18afbe8f5ff294dcb3302
nramos0 <xoen000@163.com>
2021-09-05 01:11:17 +0800

    update letsencrpy script domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(fluentreader.cc www.fluentreader.cc)
+domains=(fluentreader.cc)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="xoen000@163.com" # Adding a valid address is strongly recommended
